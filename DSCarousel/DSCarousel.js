870743f8a3ba0bcb33854dbe2efd240d977105a0
Aric <aric@firefall.io>
2019-06-24 17:29:32 -0400

    Added GSAP support, removed mutation observers, should be handled outside of script, added (untested) events

--- DSCarousel/DSCarousel.js
+++ DSCarousel/DSCarousel.js
@@ -1,12 +1,15 @@
 /* eslint complexity: "off" */
 /* Todo: Implement an update thingo? */
 
-function Conveyor(element, options) {
+function DSCarousel(element, options) {
   var _this = this;
   var defaults = {
     axis: {type: ['string', 'number', 'boolean'], default: 'horizontal'},
     reverse: {type: ['boolean'], default: false},
-    fps: {type: ['number'], default: '60'}
+    fps: {type: ['number'], default: '60'},
+    GSAP: {type: ['boolean'], default: false},
+    GSAPDuration: {type: ['number'], default: 0.5},
+    GSAPEase: {type: ['object'], default: undefined}
   };
 
   this.slides = [];
@@ -21,6 +24,17 @@ function Conveyor(element, options) {
     options[key] = defaults[key].type.indexOf(typeof options[key]) !== -1 ? options[key] : defaults[key].default;
   });
 
+  this.options = options;
+
+  this.DOMTarget = element;
+
+  // Ensure the carousel has preserve-3d
+  this.DOMTarget.transformStyle = 'preserve-3d';
+  this.DOMTarget['-webkit-transform-style'] = 'preserve-3d';
+  this.DOMTarget['-moz-transform-style'] = 'preserve-3d';
+
+  // Get axis
+  // Maybe provide and angle?
   this.axis = (function(optionDirection) {
     var direction = 'horizontal';
 
@@ -38,17 +52,17 @@ function Conveyor(element, options) {
   })(options.axis);
   this.reverse = options.reverse;
 
-  this.carousel = element;
-
-  // Ensure the carousel has preserve-3d
-  this.carousel.style.transformStyle = 'preserve-3d';
-  this.carousel.style['-webkit-transform-style'] = 'preserve-3d';
-  this.carousel.style['-moz-transform-style'] = 'preserve-3d';
+  // Get TweenMax
+  if (options.GSAP) {
+    options.GSAP = TimelineMax ? true : false;
+    this.timeline = new TimelineMax();
+  }
 
   //if (typeof MutationObserver !== 'undefined')
   // TODO: Implement shim via mutationEvents
   // Lifted straight from MDN
   // Do we listen for attribute changes?
+  /*
   this.observer = new MutationObserver(function(mutationList) {
     mutationList.forEach(function(mutation) {
       if (mutation.type === 'childList') {
@@ -56,28 +70,54 @@ function Conveyor(element, options) {
       }
     });
   });
-  this.observer.observe(this.carousel, {childList: true});
+  this.observer.observe(this.DOMTarget, {childList: true});
+  */
   this.build();
 }
 
-Conveyor.prototype = {
+DSCarousel.events = {
+  progress: new Event('DSCarousel.progress'),
+  build: new Event('DSCarousel.build')
+};
+
+DSCarousel.prototype = {
   // Used to update state of carousel based on the carousel's transform: rotateY;
   update: function() {
     // Implement
   },
   goto: function(slide) {
+    var _this = this;
+    var deltaSlide = slide - this.currentSlide;
     var axis = this.axis === 'horizontal' ? 'Y' : 'X';
 
-    slide = slide >= this.slides.length ? 0 : slide < 0 ? this.slides.length - 1 : slide;
-    this.currentSlide = slide;
-    this.angleCurrent = this.currentSlide * (this.axis === 'vertical' ? 1 : -1) * this.angleIncrement;
-    this.carousel.style.transform = 'rotate' + axis + '(' + this.angleCurrent + 'deg)';
+    if (this.options.GSAP) {
+      if (!this.timeline.isActive()) {
+        this.timeline.fromTo(this.DOMTarget, this.options.GSAPDuration, {
+          rotationY: -this.currentSlide * this.angleIncrement
+        }, {
+          rotationY: -(this.currentSlide + deltaSlide) * this.angleIncrement,
+          ease: this.options.GSAPEase,
+          onComplete: function() {
+            _this.DOMTarget.dispatchEvent(DSCarousel.events.progress, {detail: {
+              currentSlide: this.currentSlide + deltaSlide,
+              prevSlide: this.currentSlide,
+              deltaSlide: deltaSlide
+            }});
+          }
+        });
+        this.currentSlide += deltaSlide;
+      }
+    } else {
+      this.currentSlide += deltaSlide;
+      this.angleCurrent = this.currentSlide * (this.axis === 'vertical' ? 1 : -1) * this.angleIncrement;
+      this.DOMTarget.style.transform = 'rotate' + axis + '(' + this.angleCurrent + 'deg)';
+    }
   },
   next: function() {
-    this.goto(++this.currentSlide);
+    this.goto(this.currentSlide + 1);
   },
   previous: function() {
-    this.goto(--this.currentSlide);
+    this.goto(this.currentSlide - 1);
   },
   build: function() {
     var _this = this;
@@ -86,9 +126,10 @@ Conveyor.prototype = {
     var magnitude;
     var axis = this.axis === 'horizontal' ? 'Y' : 'X';
     // We assume the center is where the slide is to be displayed
+    var oldSlides = this.slides.slice(0);
 
     // Get immediate children
-    this.slides = [].slice.call(this.carousel.children, 0);
+    this.slides = [].slice.call(this.DOMTarget.children, 0);
 
     if (this.reverse) {
       this.slides.reverse();
@@ -108,7 +149,11 @@ Conveyor.prototype = {
     });
 
     // Stay on current slide, or if the current slide no longer exist, go to last available slide
-    this.currentSlide = this.currentSlide >= this.slides.length ? this.slides.length : this.currentSlide;
-    this.carousel.style.transform = 'rotate' + axis + '(' + (this.currentSlide * -this.angleIncrement) + 'deg)';
+    this.DOMTarget.style.transform = 'rotate' + axis + '(' + (this.currentSlide * -this.angleIncrement) + 'deg)';
+
+    this.DOMTarget.dispatchEvent(DSCarousel.events.build, {detail: {
+      newSlides: this.slides.slice(0),
+      oldSlides: oldSlides
+    }});
   }
 };
