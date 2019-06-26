f976f4d915d5d0b342bad6df7575ef695d70e7ca
dsmk2 <dropshipmk2@gmail.com>
2019-06-26 17:23:52 -0400

    Fleshed out dry run in build function, updated event handling

--- DSCarousel/DSCarousel.js
+++ DSCarousel/DSCarousel.js
@@ -1,110 +1,6 @@
 /* eslint complexity: "off" */
 /* global TimelineMax */
 /* Todo: Implement an update thingo? */
-
-function matrixMultiplication(multiplicand, multiplier) {
-  var result = [];
-  var sum = 0;
-  var x1 = 0;
-  var y1 = 0;
-  var x2 = 0;
-  var y2 = 0;
-
-  if (typeof multiplicand === 'undefined' || typeof multiplier === 'undefined') {
-    return;
-  }
-
-  if (multiplicand[y1].length !== multiplier.length) {
-    console.log('skipped', multiplicand, multiplier);
-    return;
-  }
-
-  // For each row of the multiplicand...
-  for (y1 = 0; y1 < multiplicand.length; y1++) {
-    result.push([]);
-    // for each column of the multiplier...
-    for (x2 = 0; x2 < multiplier[y2].length; x2++) {
-      sum = 0;
-
-      // For each column of the multiplicand
-      for (x1 = 0; x1 < multiplicand[y1].length; x1++) {
-        sum += multiplicand[y1][x1] * multiplier[x1][x2];
-      }
-
-      result[result.length - 1].push(sum);
-    }
-  }
-
-  return result;
-}
-
-function createTransformationMatrix(options) {
-  var defaults = {
-    scale: {x: 1, y: 1, z: 1},
-    rotation: {x: 0, y: 0, z: 0},
-    translation: {x: 0, y: 0, z: 0}
-  };
-  var radiansX;
-  var radiansY;
-  var radiansZ;
-  var rotationMatrixX;
-  var rotationMatrixY;
-  var rotationMatrixZ;
-  var rotationMatrix;
-  var scaleMatrix;
-  var translationMatrix;
-  var transformationMatrix;
-
-  options = Object.assign({}, defaults, options);
-
-  // Build rotation matrix
-  radiansX = options.rotation.x * Math.PI / 180;
-  radiansY = options.rotation.y * Math.PI / 180;
-  radiansZ = options.rotation.z * Math.PI / 180;
-  rotationMatrixX = [
-    [1, 0, 0, 0],
-    [0, Math.cos(radiansX), -Math.sin(radiansX), 0],
-    [0, Math.sin(radiansX), Math.cos(radiansX), 0],
-    [0, 0, 0, 1]
-  ];
-  rotationMatrixY = [
-    [Math.cos(radiansY), 0, Math.sin(radiansY), 0],
-    [0, 1, 0, 0],
-    [-Math.sin(radiansY), 0, Math.cos(radiansY), 0],
-    [0, 0, 0, 1]
-  ];
-  rotationMatrixZ = [
-    [Math.cos(radiansZ), -Math.sin(radiansZ), 0, 0],
-    [Math.sin(radiansZ), Math.cos(radiansZ), 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-  ];
-  rotationMatrix = matrixMultiplication(matrixMultiplication(rotationMatrixZ, rotationMatrixY), rotationMatrixX);
-
-  // Scale Matrix
-  scaleMatrix = [
-    [options.scale.x, 0, 0, 0],
-    [0, options.scale.y, 0, 0],
-    [0, 0, options.scale.z, 0],
-    [0, 0, 0, 1]
-  ];
-
-  // Translation Matrix
-  translationMatrix = [
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [options.translation.x, options.translation.y, options.translation.z, 1]
-  ];
-
-  // Build Transformation Matrix
-  // translate * scale * rotation (GSAP)
-  transformationMatrix = matrixMultiplication(translationMatrix, scaleMatrix);
-  transformationMatrix = matrixMultiplication(transformationMatrix, rotationMatrix);
-
-  return transformationMatrix;
-}
-
 function DSCarousel(element, options) {
   var _this = this;
   var defaults = {
@@ -178,9 +74,23 @@ function DSCarousel(element, options) {
 }
 
 DSCarousel.events = {
-  progress: new Event('DSCarousel.progress'),
-  build: new Event('DSCarousel.build')
+  progress: 'DSCarousel.progress',
+  build: 'DSCarousel.build',
+  // Shim for custom events
+  customEvent: function(eventName, options) {
+    var event = document.createEvent('Event');
+    var bubbles = typeof options.bubbles === 'boolean' ? option.bubbles : true;
+    var cancelable = typeof options.cancelable === 'boolean' ? option.cancelable : true;
+
+    event.initEvent(eventName, true, true);
+
+    event.dscarousel = options.data;
+
+    return event;
+  }
 };
+
+DSCarousel.dataAttributePrefix = 'data-dscarousel-';
 
 DSCarousel.prototype = {
   goto: function(slide) {
@@ -195,25 +105,26 @@ DSCarousel.prototype = {
         to['rotation' + axis] = (axis === 'Y' ? 1 : -1) * -(this.currentSlide + deltaSlide) * this.angleIncrement;
         to.ease = this.options.GSAPEase;
         to.onComplete = function() {
-          _this.DOMTarget.dispatchEvent(DSCarousel.events.progress, {detail: {
+          _this.DOMTarget.dispatchEvent(DSCarousel.events.customEvent(DSCarousel.events.progress, {data: {
             currentSlide: this.currentSlide - deltaSlide,
             prevSlide: this.currentSlide,
             deltaSlide: deltaSlide
-          }});
+          }}));
         };
 
         this.timeline.fromTo(this.DOMTarget, this.options.GSAPDuration, from, to);
         this.currentSlide += deltaSlide;
         this.currentSlide = this.currentSlide >= this.slides.length ? 0 : this.currentSlide < 0 ? this.slides.length - 1 : this.currentSlide;
-        console.log(this.currentSlide);
       }
     } else {
       this.currentSlide += deltaSlide;
       this.currentSlide = this.currentSlide >= this.slides.length ? 0 : this.currentSlide < 0 ? this.slides.length - 1 : this.currentSlide;
       this.angleCurrent = this.currentSlide * (this.axis === 'vertical' ? 1 : -1) * this.angleIncrement;
       this.DOMTarget.style.transform = 'rotate' + axis + '(' + this.angleCurrent + 'deg)';
-      console.log(this.currentSlide);
     }
+
+    // Update carousel angle data
+    this.DOMTarget.setAttribute(DSCarousel.dataAttributePrefix + 'angle', this.currentSlide * this.angleIncrement);
   },
   next: function() {
     this.goto(this.currentSlide + 1);
@@ -234,77 +145,83 @@ DSCarousel.prototype = {
     var magnitude;
     var axis = this.axis === 'horizontal' ? 'Y' : 'X';
     // We assume the center is where the slide is to be displayed
+    var newSlides = [];
     var oldSlides = this.slides.slice(0);
+    var newAngleIncrement;
     var dryRunResults = {
       dscarousel: {
         DOMElement: undefined,
-        transform: undefined
+        transform: undefined,
+        angleIncrementOld: this.angleIncrement,
+        currentSlide: this.currentSlide
       },
       slides: []
     };
-    var transformMatrix;
 
+    // Dry run defaults to false
     dryRun = typeof dryRun !== 'boolean' ? false : dryRun;
 
     // Get immediate children
-    this.slides = [].slice.call(this.DOMTarget.children, 0);
+    newSlides = [].slice.call(this.DOMTarget.children, 0);
 
-    this.angleIncrement = 360 / this.slides.length;
-
-    // Clear and find the largest slide based on axis
-    this.slides.forEach(function(element) {
-      var prevTransform;
-      if (dryRun) {
-        prevTransform = element.style.transform;
-      }
-
-      element.style.transform = '';
-
-      largestSize = Math.max(largestSize, _this.axis === 'horizontal' ? element.clientWidth : element.clientHeight);
-
-      if (typeof prevTransform !== 'undefined') {
-        element.style.transform = prevTransform;
-      }
+    // Only take visible slides into consideration
+    newSlides = newSlides.filter(function(element) {
+      return window.getComputedStyle(element).getPropertyValue('display') !== 'none';
     });
 
-    magnitude = (largestSize / 2 * Math.sin((90 - this.angleIncrement / 2) * RAD2DEG)) / Math.sin(this.angleIncrement / 2 * RAD2DEG);
+    // Update currentSlide if it falls outside of new slide length;
+    if (!dryRun && this.slides.length < this.currentSlide) {
+      this.currentSlide = this.slides.length - 1;
+    } else if (dryRun && this.slides.length < this.currentSlide) {
+      dryRunResults.dscarousel.currentSlide = this.slides.length - 1;
+    }
 
-    this.slides.forEach(function(element, index) {
-      transformMatrix = createTransformationMatrix({
-        rotation: {x: 0, y: (_this.axis === 'vertical' ? 1 : -1) * _this.angleIncrement * index, z: 0},
-        translation: {x: 0, y: 0, z: magnitude}
-      }).join(',');
+    newAngleIncrement = 360 / newSlides.length;
+
+    // Clear and find the largest slide based on axis, we use this to find the magnitude
+    newSlides.forEach(function(element) {
+      largestSize = Math.max(largestSize, _this.axis === 'horizontal' ? element.clientWidth : element.clientHeight);
+    });
+
+    magnitude = (largestSize / 2 * Math.sin((90 - newAngleIncrement / 2) * RAD2DEG)) / Math.sin(newAngleIncrement / 2 * RAD2DEG);
+
+    // Position each slide
+    newSlides.forEach(function(element, index) {
       if (dryRun) {
         dryRunResults.slides.push({
           DOMElement: element,
           axis: axis,
-          angle: (_this.axis === 'vertical' ? -1 : 1) * _this.angleIncrement * index,
+          angle: (_this.axis === 'vertical' ? -1 : 1) * newAngleIncrement * index,
           magnitude: magnitude,
-          transformCSS: 'rotate' + axis + '(' + ((_this.axis === 'vertical' ? -1 : 1) * _this.angleIncrement * index) + 'deg) translateZ(' + magnitude + 'px)'
+          transformCSS: 'rotate' + axis + '(' + ((_this.axis === 'vertical' ? -1 : 1) * newAngleIncrement * index) + 'deg) translateZ(' + magnitude + 'px)'
         });
       } else {
-        element.setAttribute('style', 'transform: matrix3d(' + transformMatrix + ');');
+        element.setAttribute(DSCarousel.dataAttributePrefix + 'angle', (_this.axis === 'vertical' ? -1 : 1) * newAngleIncrement * index);
+        element.setAttribute(DSCarousel.dataAttributePrefix + 'magnitude', magnitude);
+        element.style.transform = 'rotate' + axis + '(' + ((_this.axis === 'vertical' ? -1 : 1) * newAngleIncrement * index) + 'deg) translateZ(' + magnitude + 'px)';
       }
     });
 
-    transformMatrix = createTransformationMatrix({
-      rotation: {x: 0, y: this.currentSlide * this.angleIncrement, z: 0}
-    }).join(',');
-
+    // Init carousel
     if (dryRun) {
       dryRunResults.dscarousel.DOMElement = this.DOMTarget;
-      dryRunResults.dscarousel.angle = this.currentSlide * -this.angleIncrement;
+      dryRunResults.dscarousel.angleOld = this.angleCurrent;
+      dryRunResults.dscarousel.angleNew = dryRunResults.dscarousel.currentSlide * newAngleIncrement;
+      dryRunResults.dscarousel.angleIncrementNew = this.angleIncrement;
       dryRunResults.dscarousel.axis = axis;
-      dryRunResults.dscarousel.transformCSS = 'matrix3d(' + transformMatrix + ')';
+      dryRunResults.dscarousel.transformCSS = 'rotate' + axis + '(' + this.currentSlide * this.angleIncrement + 'deg);';
       return dryRunResults;
     } else {
-      // Stay on current slide, or if the current slide no longer exist, go to last available slide
-      this.DOMTarget.style.transform = 'matrix3d(' + transformMatrix + ')';
+      this.angleIncrement = newAngleIncrement;
+      this.slides = newSlides;
+      this.DOMTarget.setAttribute(DSCarousel.dataAttributePrefix + 'angle', this.currentSlide * this.angleIncrement);
 
-      this.DOMTarget.dispatchEvent(DSCarousel.events.build, {detail: {
+      // Stay on current slide, or if the current slide no longer exists, go to last available slide
+      this.DOMTarget.style.transform = 'rotate' + axis + '(' + this.currentSlide * this.angleIncrement + 'deg);';
+      this.DOMTarget.dispatchEvent(DSCarousel.events.customEvent(DSCarousel.events.build, {data: {
         newSlides: this.slides.slice(0),
         oldSlides: oldSlides
-      }});
+      }}));
     }
   },
   // Gets the current angle on the current axis and updates currentSlide to match
